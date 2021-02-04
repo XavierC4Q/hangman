f76324e81ec425ef347862da897a9f7bd7845068
Xavier Munroe <munroexavier@gmail.com>
2021-02-03 22:53:08 -0500

    letter buttons

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ class Game {
     try {
       const res = await fetch("https://random-words-api.vercel.app/word");
       const data = await res.json();
-      const word = data[0].word.toLowerCase();
+      const word = data[0].word.toUpperCase();
       this.word = word;
       this.progress = word.split("").map(() => null);
     } catch (e) {
@@ -43,4 +43,28 @@ class Game {
 document.addEventListener("DOMContentLoaded", async () => {
   const game = new Game();
   await game.init();
+
+  const selections = document.getElementById("selections");
+  selections.append(...createLetterSelections(game));
 });
+
+
+
+// LETTER BUTTONS
+function createLetterSelections(game) {
+  const alphabet = [...Array(26)]
+    .map((_, y) => String.fromCharCode(y + 65))
+    .join("");
+
+  return alphabet.split('').map((letter) => {
+    const btn = document.createElement("button");
+    btn.type = "button";
+    btn.className = "letter-select";
+    btn.innerText = letter;
+    btn.addEventListener("click", () => {
+      game.guess(letter);
+      btn.disabled = true;
+    });
+    return btn;
+  });
+}
