dea9554e5b2e311a5a52a09f7f4c578557226359
Xavier Munroe <munroexavier@gmail.com>
2021-02-04 22:00:56 -0500

    word display section

--- main.js
+++ main.js
@@ -25,6 +25,8 @@ class Game {
       this.word.split("").forEach((w, i) => {
         if (w === letter) {
           this.progress[i] = letter;
+          const letterDisplay = document.getElementById(`idx-${i}`);
+          letterDisplay.innerText = w;
         }
       });
     }
@@ -44,11 +46,30 @@ document.addEventListener("DOMContentLoaded", async () => {
   const game = new Game();
   await game.init();
 
+  const wordDisplay = document.getElementById("word");
+  wordDisplay.append(...createWordDisplay(game));
+
   const selections = document.getElementById("selections");
   selections.append(...createLetterSelections(game));
 });
 
+// WORD DISPLAY
+/**
+ *
+ * Going to need a way to tag these elements so they can be dynamically updated
+ */
+function createWordDisplay(game) {
+  const word = game.word;
 
+  return word.split("").map((w, i) => {
+    const letterDisplay = document.createElement("span");
+    letterDisplay.id = `idx-${i}`;
+    letterDisplay.className = "letter-display";
+    letterDisplay.innerText = "_";
+
+    return letterDisplay;
+  });
+}
 
 // LETTER BUTTONS
 function createLetterSelections(game) {
@@ -56,7 +77,7 @@ function createLetterSelections(game) {
     .map((_, y) => String.fromCharCode(y + 65))
     .join("");
 
-  return alphabet.split('').map((letter) => {
+  return alphabet.split("").map((letter) => {
     const btn = document.createElement("button");
     btn.type = "button";
     btn.className = "letter-select";
